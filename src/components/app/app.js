5662e9130adfdc28bc7b323fa0ea0d75303ef834
Yuliya Reutovich <yrevtovich@mail.ru>
2020-09-20 12:01:56 +0300

    feat: add task  structures

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,7 +7,7 @@ import MainTable from '../main-table/main-table';
 
 export default class App extends Component {
   state = {
-    isTaskPageOpen: true,
+    isTaskPageOpen: false,
     data: {},
   }
 
--- src/configs/taskStructure.js
+++ src/configs/taskStructure.js
@@ -14,7 +14,6 @@ const taskStructure = {
     taskDescription: true,
     map: true,
     organizer: true,
-    // organizerInfo: true,
     feedback: true,
   },
 
@@ -33,11 +32,82 @@ const taskStructure = {
     taskDescription: true,
     map: false,
     organizer: true,
-    // organizerInfo: true,
+    feedback: true,
+  },
+
+  htmltask: {
+    type: true,
+    header: true,
+    date: {      
+      isStartDate: true,
+      isStartTime: false,
+      isDeadlineDate: true,
+    },
+    lectureDescription: false,
+    image: true,
+    video: false,
+    link: true,
+    taskDescription: true,
+    map: false,
+    organizer: true,
+    feedback: true,
+  },
+
+  codejam: {
+    type: true,
+    header: true,
+    date: {      
+      isStartDate: true,
+      isStartTime: false,
+      isDeadlineDate: true,
+    },
+    lectureDescription: false,
+    image: true,
+    video: false,
+    link: true,
+    taskDescription: true,
+    map: false,
+    organizer: true,
+    feedback: true,
+  },
+
+  externaltask: {
+    type: true,
+    header: true,
+    date: {      
+      isStartDate: true,
+      isStartTime: false,
+      isDeadlineDate: true,
+    },
+    lectureDescription: false,
+    image: true,
+    video: false,
+    link: true,
+    taskDescription: true,
+    map: false,
+    organizer: true,
     feedback: true,
   },
 
   lectureOnline: {
+    type: true,
+    header: true,
+    date: {      
+      isStartDate: true,
+      isStartTime: true,
+      isDeadlineDate: false,
+    },
+    lectureDescription: true,
+    image: false,
+    video: false,
+    link: true,
+    taskDescription: false,
+    map: true,
+    organizer: true,
+    feedback: true,
+  },
+
+  lectureOffline: {
     type: true,
     header: true,
     date: {      
@@ -52,9 +122,26 @@ const taskStructure = {
     taskDescription: false,
     map: false,
     organizer: true,
-    // organizerInfo: true,
     feedback: true,
-  }
+  },
+
+  codewars: {
+    type: true,
+    header: true,
+    date: {      
+      isStartDate: true,
+      isStartTime: false,
+      isDeadlineDate: true,
+    },
+    lectureDescription: false,
+    image: false,
+    video: false,
+    link: true,
+    taskDescription: true,
+    map: false,
+    organizer: true,
+    feedback: true,
+  },
 }
   
 export default taskStructure;
